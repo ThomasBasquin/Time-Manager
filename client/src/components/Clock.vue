a95dcee77b580e0af21665a999df8cc213023b84
Thomas <thomas.basquin2@gmail.com>
2023-10-25 16:59:43 +0200

    fix catch

--- client/src/components/Clock.vue
+++ client/src/components/Clock.vue
@@ -1,27 +1,70 @@
-<script setup lang="ts">
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-  import { Input } from "@/components/ui/input";
+<script setup>
+  import axios from "axios";
+  import { store } from "@/store";
+
   import { Button } from "@/components/ui/button";
   import { ref } from "vue";
 
-  const working = ref(false);
-  const dateStart = ref(new Date());
-  const dateEnd = ref(new Date());
+  console.log(store.user);
 
-  function toggleWorkStatus() {
+  const working = ref(false);
+  let dateStart = ref(new Date());
+  let dateEnd = ref(new Date());
+  let userId = null;
+
+  async function toggleWorkStatus() {
     working.value = !working.value;
     if (working.value) {
-      dateEnd.value = null;
       dateStart.value = new Date();
+      try {
+        let response = await axios.post("/api/clocks", {
+          clock: {
+            user_id: 1,
+            user: {
+              username: "mxttmg",
+              email: "matteo.murgolo@epitech.eu",
+            },
+            time: dateStart.value.toISOString(),
+            status: true,
+          },
+        });
+        dateStart.value = response.data.time;
+        userId = response.data.id;
+      } catch (error) {
+        console.log(error);
+      }
     } else {
-      dateEnd.value = new Date();
+      try {
+        dateEnd.value = new Date();
+        let response = await axios.get("/api/clocks", {
+          params: { userId: 1 },
+        });
+        let clockData = response.data;
+
+        if (clockData.status) {
+          let clockInfo = {
+            id: clockData.id,
+            time: clockData.time,
+            status: clockData.status,
+            user_id: clockData.user_id,
+          };
+          // TODO Créer un working time
+
+          await axios.post("/api/clocks", {
+            clock: {
+              user_id: 1,
+              user: {
+                username: "mxttmg",
+                email: "matteo.murgolo@epitech.eu",
+              },
+              time: dateEnd.value.toISOString(),
+              status: false,
+            },
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 </script>
