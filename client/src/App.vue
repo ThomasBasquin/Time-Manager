5813f87f99724cc31535a4fa0b532cc0064d4b69
Matteo Murgolo <matteo.murgolo@epitech.eu>
2023-10-25 11:12:14 +0200

    add: Login component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import Login from '@/components/Login.vue'
 </script>
 
 <template>
-  <Button>Click me</Button>
+  <div>
+    <Login />
+  </div>
 </template>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+
+function increment() {
+    state.count++
+    console.log(state.count)
+}
+</script>
+
+<template>
+    <Card v-if="state.count == 0" class="w-1/4 h-2/4">
+        <CardHeader>
+            <CardTitle>Login to your user</CardTitle>
+            <CardDescription>Enter your mail to login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Input type="email" placeholder="Email" />
+        </CardContent>
+        <CardFooter class="flex flex-row-reverse">
+            <Button>Login</Button>
+            <Button class="mr-2" @click="increment">Create account</Button>
+        </CardFooter>
+    </Card>
+    <Card v-if="state.count == 1" class="w-1/4 h-2/4">
+        <CardHeader>
+            <CardTitle>Create a account</CardTitle>
+            <CardDescription>Enter your mail to create an account</CardDescription>
+        </CardHeader>
+        <CardContent class="space-y-2">
+            <Input type="text" placeholder="Username" />
+            <Input type="email" placeholder="Email" />
+        </CardContent>
+        <CardFooter class="flex flex-row-reverse">
+            <Button class="mr-2" @click="state.count = 0">Create account</Button>
+        </CardFooter>
+    </Card>
+</template>
