8ed8829305efc42dfe7f434a99dec2ab7ceec9a4
Matteo Murgolo <matteo.murgolo@epitech.eu>
2023-10-25 10:17:04 +0200

    init: vue frontend with shadcn ui

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,7 @@
-<script setup>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Button>Click me</Button>
 </template>
