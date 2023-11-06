64b649c2e92a335bfbf14ecf15ddf65a6b632b80
Thomas <thomas.basquin2@gmail.com>
2023-11-06 14:22:25 +0100

    Add VitePWA plugin to vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,39 @@
-import path from "path"
-import vue from "@vitejs/plugin-vue"
-import { defineConfig } from "vite"
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Time manager",
+        short_name: "TM",
+        description:
+          "L'application Time Manager est conçue pour optimiser la gestion du temps de travail au cœur de Gotham City",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "time-manager-icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "time-manager-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
